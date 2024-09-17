02c8e215a2bcd69484f19cded7f6adcea86c0877
Sadık Orak <muhammedsadikorak@gmail.com>
2024-09-17 12:57:06 +0300

    Entity ve IEntity sınıfları oluşturuldu.

---
+++ Source/TaxiApp.Domain/Entities/Users/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApp.Domin.Core.EntitiesCore;
+
+namespace TaxiApp.Domain.Entities.Users
+{
+  public class User : Entity
+  {
+  }
+}
