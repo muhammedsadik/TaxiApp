b600690f90bc0635d1a5720533fc71382f014bc8
Sadık Orak <muhammedsadikorak@gmail.com>
2024-11-02 11:49:03 +0300

    Utilities -> Results: Error handling created. Result and Dataresult created.

---
+++ Source/TaxiApp.Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp.Core.Utilities.Results
+{
+  public class DataResult<TEntity> : Result, IDataResult<TEntity>
+  {
+    public DataResult(TEntity data, bool isSuccess, string message) : base(isSuccess, message)
+    {
+      Data = data;
+    }
+
+    public DataResult(TEntity data, bool isSuccess) : base(isSuccess)
+    {
+      Data = data;
+    }
+
+    public TEntity Data { get; }
+  }
+}
